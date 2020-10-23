c1943301793b0a2ecd43ed8af82022e807ad0495
doublegrey <sergejkropinov@gmail.com>
2020-10-23 11:38:56 +0300

    rest handler

--- rest/rest.go
+++ rest/rest.go
@@ -6,10 +6,13 @@ import (
 	"log"
 	"net/http"
 	"strings"
+
+	"github.com/doublegrey/microlog/db"
+	"github.com/doublegrey/microlog/utils"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	topic := r.Header.Get("Topic")
+	topic := strings.TrimSpace(r.Header.Get("Topic"))
 	if len(strings.TrimSpace(topic)) < 1 {
 		fmt.Fprintf(w, "'Topic' field does not exist or is empty")
 		return
@@ -18,6 +21,17 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Fprintf(w, "Failed to read request body: %v", err)
 		log.Printf("Failed to read request body: %v\n", err)
+		return
+	}
+	if len(topic) > 0 && utils.Config.Kafka.CustomTopics {
+		topic = fmt.Sprintf("%s_%s", utils.Config.Kafka.TopicPrefix, topic)
+	} else {
+		topic = utils.Config.Kafka.Topic
+	}
+	err = db.Produce(b, topic)
+	if err != nil {
+		fmt.Fprintf(w, "Failed to produce message: %v", err)
+		log.Printf("Failed to produce message: %v\n", err)
 		return
 	}
 }
