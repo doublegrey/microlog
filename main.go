a40f75248c7183ebbba72cc6e6c972efb5dd4014
doublegrey <sergejkropinov@gmail.com>
2020-11-06 10:40:28 +0200

    remove kafka handlers

--- main.go
+++ main.go
@@ -2,39 +2,12 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
 
-	"github.com/doublegrey/microlog/db"
 	"github.com/doublegrey/microlog/utils"
 )
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	topic := strings.TrimSpace(r.Header.Get("Topic"))
-	if len(strings.TrimSpace(topic)) < 1 {
-		fmt.Fprintf(w, "'Topic' field does not exist or is empty")
-		return
-	}
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "Failed to read request body: %v", err)
-		log.Printf("Failed to read request body: %v\n", err)
-		return
-	}
-	if len(topic) > 0 && utils.Config.Kafka.CustomTopics {
-		topic = fmt.Sprintf("%s_%s", utils.Config.Kafka.TopicPrefix, topic)
-	} else {
-		topic = utils.Config.Kafka.Topic
-	}
-	err = db.Produce(b, topic)
-	if err != nil {
-		fmt.Fprintf(w, "Failed to produce message: %v", err)
-		log.Printf("Failed to produce message: %v\n", err)
-		return
-	}
-}
 
 func validateToken(handler func(http.ResponseWriter, *http.Request)) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -52,11 +25,6 @@ func main() {
 	err := utils.Config.Parse()
 	if err != nil {
 		log.Fatalf("Failed to parse config: %v\n", err)
-	}
-	if utils.Config.Auth {
-		http.Handle("/", validateToken(handler))
-	} else {
-		http.HandleFunc("/", handler)
 	}
 
 	err = http.ListenAndServe(fmt.Sprintf("%s:%d", utils.Config.Host, utils.Config.Port), nil)
--- utils/config.go
+++ utils/config.go
@@ -9,23 +9,6 @@ type config struct {
 	Port  int
 	Auth  bool
 	Token string
-	Kafka kafka
-	UI    ui
-}
-
-type kafka struct {
-	Brokers      []string
-	Topic        string
-	CustomTopics bool   `toml:"allow_custom_topic"`
-	TopicPrefix  string `toml:"custom_topic_prefix"`
-}
-
-type ui struct {
-	Enabled  bool
-	Host     string
-	Port     int
-	Username string
-	Password string
 }
 
 func (c *config) Parse() error {
